d5383ec173a20ce51c5b5ae5f3d1f684a8fec4e6
Noe De Santo <noe@ef5.ch>
2025-03-16 13:37:34 -0400

    Remove extraneous `SNatI` constrains

--- src/AutoEnv/Bind/Local.hs
+++ src/AutoEnv/Bind/Local.hs
@@ -1,7 +1,7 @@
 -- | single binder, but includes a name for pretty printing
 -- type synonym for Pat.Bind with an opaque name as the pattern
 module AutoEnv.Bind.Local
-   (module AutoEnv.Classes,
+  ( module AutoEnv.Classes,
     type Bind,
     bind,
     getLocalName,
@@ -9,12 +9,13 @@ module AutoEnv.Bind.Local
     getBody,
     unbind,
     instantiate,
-    applyUnder
-    ) where
+    applyUnder,
+  )
+where
 
 import AutoEnv
+import AutoEnv.Bind.Pat qualified as Pat
 import AutoEnv.Classes
-import qualified AutoEnv.Bind.Pat as Pat
 
 ---------------------------------------------------------------
 -- LocalBind operations (convenience wrappers)
@@ -26,13 +27,13 @@ type Bind v c n = Pat.Bind v c LocalName n
 -- deriving instance (FV (Bind v c))
 -- deriving instance (Strengthen (Bind v c))
 
-bind :: (SNatI n, Subst v c) => LocalName -> c (S n) -> Bind v c n
+bind :: (Subst v c) => LocalName -> c (S n) -> Bind v c n
 bind = Pat.bind
 
 getLocalName :: Bind v c n -> LocalName
 getLocalName = Pat.getPat
 
-internalBind :: (SNatI n, Subst v c) => c (S n) -> Bind v c n
+internalBind :: (Subst v c) => c (S n) -> Bind v c n
 internalBind = Pat.bind internalName
 
 getBody :: (Subst v v, Subst v c) => Bind v c n -> c (S n)
@@ -40,15 +41,18 @@ getBody = Pat.getBody
 
 -- unbind, but also provide access to the local name
 -- if one is available
-unbind :: (Subst v v, Subst v c) => Bind v c n 
-  -> ((LocalName , c (S n)) -> d) -> d
+unbind ::
+  (Subst v v, Subst v c) =>
+  Bind v c n ->
+  ((LocalName, c (S n)) -> d) ->
+  d
 unbind b f = f (getLocalName b, getBody b)
 
 instantiate :: (Subst v c) => Bind v c n -> v n -> c n
 instantiate b e = Pat.instantiate b (oneE e)
 
 applyUnder ::
-  (SNatI n2, Subst v v, Subst v c) =>
+  (Subst v v, Subst v c) =>
   (forall m n. Env v m n -> c m -> c n) ->
   Env v n1 n2 ->
   Bind v c n1 ->
